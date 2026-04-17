5eda83e31a5c08195ad21ab0e935123f23b09ca4
Никита <nikita22myt@gmail.com>
2026-04-17 16:41:01 +0300

    Add files via upload

--- js/onesignal-init.js
+++ js/onesignal-init.js
@@ -8,7 +8,7 @@ OneSignalDeferred.push(async function (OneSignal) {
     await OneSignal.init({
         appId: 'd496aabd-b8cf-4d18-946b-8228b23392f7',
         serviceWorkerPath: 'sw.js',
-        serviceWorkerParam: { scope: '/' },
+        serviceWorkerParam: { scope: '/prompt/' },
         notifyButton: { enable: false },
         autoPrompt: false,
         allowLocalhostAsSecureOrigin:
